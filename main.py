bfde84a43340355556ac7ef4103b4c4b6cc65445
TahaHachana <tahahachana@gmail.com>
2024-09-14 15:15:09 +0100

    move player spaceship

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -14,10 +20,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
         delta = clock.tick(60)
         dt = delta / 1000
+        for d in drawable:
+            d.draw(screen)
+        for u in updatable:
+            u.update(dt)
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
