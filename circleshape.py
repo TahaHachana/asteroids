cdb89fe653b4a5299c6cdc9e7581bff5b17e74ba
TahaHachana <tahahachana@gmail.com>
2024-09-17 16:57:21 +0100

    shooting

--- circleshape.py
+++ circleshape.py
@@ -22,8 +22,4 @@ class CircleShape(pygame.sprite.Sprite):
         pass
 
     def collides_with(self, other):
-        distance = self.position.distance_to(other.position)
-        if distance < self.radius + other.radius:
-            return True
-        else:
-            return False
+        return self.position.distance_to(other.position) <= self.radius + other.radius
